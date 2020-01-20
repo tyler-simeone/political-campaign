75407ea8451f4d8d9f53d8a01cfed9920e024a0f
Tyler Simeone <tyler.simeone@gmail.com>
2020-01-20 10:02:41 -0600

    added object for political candidate

---
+++ src/scripts/main.js
@@ -0,0 +1,28 @@
+const politicalCandidate = {
+    district: "7th Congressional district of Tennessee.",
+    platformStatements: {
+        taxes: " ",
+        jobs: " ",
+        infrastructure: " ",
+        healthCare: " ",
+        crimeAndEnforcement: " "
+    },
+    urlDonationForm: ,
+    eventsCalender: [],
+    volunteerInfo: {
+        name: " ",
+        address: " ",
+        email: " ",
+        phoneNumber: " ",
+        availability: ,
+        activity: " "
+    },
+    biography: ,
+    imageGallery: {
+        headShot: ,
+        familyPicture: ,
+        constituentsPicture: ,
+    },
+    missionStatement: " ",
+    urlVoterRegistration: 
+};
